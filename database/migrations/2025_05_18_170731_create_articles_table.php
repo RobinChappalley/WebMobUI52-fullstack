bae6856a2161b139e0aa82303a8df1d7cda28631
Robin Chappalley <robin@chapi.ch>
2025-05-18 19:18:11 +0200

    update migration

--- database/migrations/2025_05_18_170731_create_articles_table.php
+++ database/migrations/2025_05_18_170731_create_articles_table.php
@@ -6,18 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('description');
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('articles');
     }
-}; 
+};
